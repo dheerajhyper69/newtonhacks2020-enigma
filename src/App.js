cc2ddbf78bd9b7474366d8e5590fb6170c5d7686
Dheeraj Prakash <dheerajpv16@gmail.com>
2020-08-29 18:46:31 +0000

    doing stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Input from './components/Input';
 import Rotor from './Rotor';
+
+const slowRotor = ["B", "D", "F", "H", "J", "L", "C", "P","R", "T", "X", "V", "Z","N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"];
+
+const medRotor = ["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"];
+
+const fastRotor = ["E", "K", "M", "F","L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"];
 
 class App extends React.Component {
   constructor(props) {
@@ -14,16 +19,28 @@ class App extends React.Component {
     };
   }
 
+  isLetter = (str) => {
+    return str.length === 1 && (str.match(/[a-z A-Z]/i));
+  }
+
   handleTextChange = (e) => {
-    this.setState({ plainText: e.target.value });
-    this.setState({ cipherText: Rotor.encoder(this.state.plainText) });
+    let symbollessString = '';
+
+    for(var i = 0; i < e.target.value.length; i++) {
+      if (this.isLetter(e.target.value.charAt(i))) symbollessString = symbollessString + e.target.value.charAt(i);
+    }
+
+    this.setState({ plainText: symbollessString });
+    this.setState({ cipherText: Rotor.encoder(Rotor.encoder(Rotor.encoder(Rotor.reflectorEncoder(Rotor.encoder(Rotor.encoder(Rotor.encoder(this.state.plainText, slowRotor), medRotor), fastRotor)), slowRotor), medRotor), fastRotor) });
   }
 
   render() {
       return (
         <div className="App">
-          <Input handleTextChange={this.handleTextChange} />
+          <p> Play around with the Enigma and see what you can come up with! </p>
+          <input onChange={this.handleTextChange} placeholder="Enter plaintext here..."/>
           <p>{this.state.cipherText}</p>
+          <footer>*press space at the end of your string*</footer>
         </div>
       );
     }
--- src/Rotor.js
+++ src/Rotor.js
@@ -1,10 +1,10 @@
 //import 
 // military grade Enigma rotors
-const fastRotor = ["B", "D", "F", "H", "J", "L", "C", "P","R", "T", "X", "V", "Z","N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"];
+const slowRotor = ["B", "D", "F", "H", "J", "L", "C", "P","R", "T", "X", "V", "Z","N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"];
 
-const medRotor = ["A", "J", "D", "K", "S", "F", "I", "R", "U", "X", "B", "L", "H", "w", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"];
+const medRotor = ["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"];
 
-const slowRotor = ["E", "K", "M", "F","L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"];
+const fastRotor = ["E", "K", "M", "F","L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"];
 
 
 // Reflector needed to return the ciphertext back through the three rotors
@@ -12,33 +12,45 @@ const reflector = ["F", "V", "P", "J", "I", "A", "O", "Y", "E", "D", "R", "Z", "
 
 // Main class that does the encrypting 
 export default class Rotor {
-    static encoder = (plaintext) => { 
+    static encoder = (plaintext, rotor) => { 
       let cipher = plaintext.toUpperCase();
-      let cipherText = [];
       let num = 0;
       let encoded_word = " ";
 
-      for (var i = 0; i < cipher.length; i++) {
-        cipherText[i] = cipher[i];
-      }
-
-      for (var d = 0; d < cipherText.length; d++ ){
+      for (var d = 0; d < cipher.length; d++ ){
         num = cipher.charCodeAt(d);
-        if (num === 32) {
-          encoded_word = encoded_word+ " ";
+          if (num === 32) {
+           encoded_word = encoded_word + " ";
+          }
+        // takes the letter in the array corresponding to the plaintext
+        encoded_word += rotor[num - 65]; 
+        
+      }
+      // var tempLet = rotor.shift();
+      //   rotor.push(tempLet);
+
+     return encoded_word.substring(1, encoded_word.length).replaceAll('NWKSFUWSK', '').replaceAll('undefined', '');
+    }
+
+    static reflectorEncoder = (plaintext) => { 
+      let cipher = plaintext.toUpperCase();
+      let num = 0;
+      let encoded_word = " ";
+
+      for (var d = 0; d < cipher.length; d++ ){
+        num = cipher.charCodeAt(d);
+        
+         if (num === 32) {
+           encoded_word = encoded_word + " ";
         }
       
         // takes the letter in the array corresponding to the plaintext
-        encoded_word += fastRotor[num - 65];
-
-        //shift part 
-        let firstVal = 
-        fastRotor.concat(fastRotor.shift());
+        encoded_word += reflector[num - 65];
+        
       }
-      
 
-    return encoded_word;
+     return encoded_word.substring(1, encoded_word.length).replaceAll('NWKSFUWSK', '').replaceAll('undefined', '');
     }
-}
 
- 
+
+}
